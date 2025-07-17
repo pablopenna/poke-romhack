45d9d6511fda7b5faf75645b4c0603548d0ecea0
Pablo Pena <pablo_penna7@hotmail.com>
2025-07-17 10:57:01 +0800

    feat: add Team Rocket trainer class

--- src/battle_main.c
+++ src/battle_main.c
@@ -370,6 +370,7 @@ const struct TrainerClass gTrainerClasses[TRAINER_CLASS_COUNT] =
     [TRAINER_CLASS_PIKE_QUEEN] = { _("PIKE QUEEN") },
     [TRAINER_CLASS_PYRAMID_KING] = { _("PYRAMID KING") },
     [TRAINER_CLASS_RS_PROTAG] = { _("{PKMN} TRAINER") },
+    [TRAINER_CLASS_TEAM_ROCKET] = { _("TEAM ROCKET") },
 };
 
 static void (*const sTurnActionsFuncsTable[])(void) =
